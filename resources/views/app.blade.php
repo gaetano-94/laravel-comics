1219c924b7f4d0efd100ccd3df699f73c602f24d
gaetano-94 <gaetano.panico1994@gmail.com>
2024-01-28 10:12:58 +0100

    feat: fix scaffolding piu img

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>DC Comics</title>
+  {{-- inclusione file JS --}}
+  @vite('resources/js/app.js')
+  {{-- /inclusione file JS --}}
+</head>
+<body>
+  
+</body>
+</html>
